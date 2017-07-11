129e1191a9adb97335a6e4c7a65ced82af2d1b5d
nextsmsversion <nextsmsversion@gmail.com>
2017-07-11 15:53:54 +0800

    PA prototype v1.0 complete

--- webui/Devices/bundle/webapp/js/controllers.js
+++ webui/Devices/bundle/webapp/js/controllers.js
@@ -75,3 +75,17 @@ devicesControllers.controller('SessionCtrl', ['$scope', '$http',
       }
     });
   }]);
+
+var app = angular.module('myApp', []);
+
+app.controller('TabController', function () {
+    this.tab = 1;
+
+    this.setTab = function (tabId) {
+        this.tab = tabId;
+    };
+
+    this.isSet = function (tabId) {
+        return this.tab === tabId;
+    };
+});
