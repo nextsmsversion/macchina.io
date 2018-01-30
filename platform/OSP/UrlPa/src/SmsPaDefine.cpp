548467122599bef2040b3995116a2cd8442ce167
nextsmsversion <nextsmsversion@gmail.com>
2018-01-30 17:31:34 +0800

    PA 20180130 decodeMsgType

---
+++ platform/OSP/UrlPa/src/SmsPaDefine.cpp
@@ -0,0 +1,16 @@
+
+// Message configuration file section
+#define PA_SECTION_UNKNOWN			0
+#define PA_SECTION_MSG_TYPE			1
+#define PA_SECTION_MSG_VAR			2
+#define PA_SECTION_PREDEF_MSG		3
+
+// Predefined message line type
+#define PA_MSG_LINE_TYPE_UNKNOWN	0
+#define PA_MSG_LINE_WITHOUT_VAR		1
+#define PA_MSG_LINE_WITH_VAR		2
+#define PA_MSG_LINE_WITH_VAR_END	3
+
+// Message type id
+#define PA_MIN_MSG_TYPE_ID			1
+#define PA_MAX_MSG_TYPE_ID			11
--- platform/OSP/UrlPa/src/UrlPa.cpp
+++ platform/OSP/UrlPa/src/UrlPa.cpp
@@ -20,6 +20,7 @@
 #include "Poco/StringTokenizer.h"
 #include "Poco/ClassLibrary.h"
 #include "Poco/MD5Engine.h"
+#include "SmsPaDefine.cpp"
 #include <set>
 
 //added by sam 20171010 START for adding push notification
@@ -79,6 +80,198 @@ using Poco::AutoPtr;
 using Poco::StringTokenizer;
 
 
+class SmsFilePaMgr
+{
+
+public :
+	//TODO by sam static bool ReadZoneConfig( const char *fileName ) ;
+	static bool ReadMsgConfig( const char *fileName );
+	//TODO by sam static bool ReadInstantMsgConfig( const char *fileName ) ;
+	//TODO by sam static bool ReadMhqueue( const char *fileName ) ;
+	//TODO by sam static bool ReadSchedule( const char *fileName, long schedDay ) ;
+
+private :
+	static bool AnalyseMsgConfigLine( string line, long msgConfigSection ) ;
+	static bool AnalyseMsgTypeLine( string line ) ;
+	//TODO by sam static bool AnalyseMsgVarLine( RWCString line ) ;
+	//TODO by sam static bool AnalysePredefMsgLine( RWCString line, long predefMsgLineType ) ;
+	//TODO by sam static bool AnalysePredefMsgLineWithoutVar( RWCString line ) ;
+	//TODO by sam static bool AnalysePredefMsgLineWithVar( RWCString line ) ;
+	//TODO by sam static bool AnalysePredefMsgLineWithVarEnd( RWCString line ) ;
+
+	//TODO by sam static void SendCommand( long cmdId, IlsMvValue param1 = IlsMvValue(), IlsMvValue param2 = IlsMvValue(),
+	//TODO by sam 	IlsMvValue param3 = IlsMvValue(), IlsMvValue param4 = IlsMvValue(), IlsMvValue param5 = IlsMvValue(),
+	//TODO by sam 	IlsMvValue param6 = IlsMvValue(), IlsMvValue param7 = IlsMvValue(), IlsMvValue param8 = IlsMvValue() ) ;
+
+	//TODO by sam static bool BuildMask( const char *physicalAddressLine, RWCString &mask, int &physicalAddress ) ;
+	//TODO by sam static RWCString ReadTitle( RWCString &text ) ;
+	//TODO by sam static bool DecodeMsgCode( const char *msgCode, RWCString &zoneAddress, bool &isPhysicalAddress, RWCString &messageField ) ;
+
+	//------------------------------------------------------------------------------------------------------------------
+	//#BEGIN
+	//#ATTRIBUT static long OpenSection
+	//#COMMENT Part of Pa database file for which the 'transaction' has been open.
+	//#END
+	//------------------------------------------------------------------------------------------------------------------
+	//static long OpenSection ;
+
+	//------------------------------------------------------------------------------------------------------------------
+	//#BEGIN
+	//#ATTRIBUT static long LastPredefMsgLineType
+	//#COMMENT Type of last predefined message line successfully analysed.
+	//#END
+	//------------------------------------------------------------------------------------------------------------------
+	//static long LastPredefMsgLineType ;
+} ;
+
+//----------------------------------------------------------------------------------------------------------------------
+//#BEGIN
+//#METHOD bool SmsFilePaMgr::ReadMsgConfig( const char *fileName )
+//#COMMENT This method is used to read Pa database file. This file is devided into three part : message types
+// description part, message variables description part and predefined messages part. A command is sent to server for
+// each message types, message variables or predefined messages described in file. These commands are sent in three
+// different 'transactions'. For each 'transaction', a first command is sent to start transaction, then commands are
+// sent and a last command is sent to close transaction. The aim of these transactions is to know which message types,
+// message variables and predefined messages are no more described in Pa database file and must be deleted.
+//#END
+//----------------------------------------------------------------------------------------------------------------------
+bool SmsFilePaMgr::ReadMsgConfig( const char *fileName )
+{
+	printf("SmsFilePaMgr::ReadZoneConfig");
+
+	// Open zone configuration file
+    ifstream input( fileName ) ;
+	if ( !input )
+	{
+		cerr << "*** Cannot open file %s ***", fileName  ;
+		return false ;
+	}
+
+	else{
+		cerr << "*** SmsFilePaMgr::ReadMsgConfig ifstream input file successfully" ;
+	}
+
+	long OpenSection = PA_SECTION_MSG_TYPE ;
+	long LastPredefMsgLineType = PA_MSG_LINE_WITHOUT_VAR ;
+
+	string line;
+
+	while (std::getline(input, line)){
+
+
+		if ( !AnalyseMsgConfigLine( line, OpenSection ) )
+		{
+			cerr << "*** Syntax error ***" ;
+			cerr << "Analysed line : " << line << std::endl;
+		}
+	}
+
+	return true;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//#BEGIN
+//#METHOD bool SmsFilePaMgr::AnalyseMsgConfigLine( RWCString line, long msgConfigSection )
+//#COMMENT This method is used to analyse Pa database file line. This line will be analysed as if it belongs to opened
+// section (current transaction). If it does not fit such line, it will be analysed as if it belongs to following
+// sections. If it still does not succeed in any following sections, this line will be thought to have a syntax error
+// and next line will be analysed (as opened section line). If it succeeds in a following section, opened section will
+// be closed and new section will be opened (new transaction).
+// NB : Section order is : 1. message types,
+//                         2. message variables,
+//                         3. predefined messages.
+//#END
+//----------------------------------------------------------------------------------------------------------------------
+bool SmsFilePaMgr::AnalyseMsgConfigLine( string line, long msgConfigSection )
+{
+	bool success ;
+
+	// Switch on section this line is supposed to belong to
+	switch ( msgConfigSection )
+	{
+		case PA_SECTION_MSG_TYPE :
+			success = AnalyseMsgTypeLine( line ) ;
+			break ;
+
+/**
+ * 		traditionally, it becomes object "msg = new SmsPaPredefMsg( msgId, msgName, firstPartText, firstPartVar ) ;"
+ * 		ILS_RP_ATTR_STRING( SmsRpPaMsgPart, msgPartText, setText )
+ *		next step is to make it a rest API w.r.t.
+ *		TODO 20180130
+ *		1) getPaStatusJson
+ *		2) serviceRegistry.find('io.macchina.instancemsg != ""') & devices.jss : response.write(JSON.stringify(devices));
+ *
+ *
+ *
+ *
+		case PA_SECTION_MSG_VAR :
+			success = AnalyseMsgVarLine( line ) ;
+			break ;
+
+		case PA_SECTION_PREDEF_MSG :
+			success = AnalysePredefMsgLine( line, LastPredefMsgLineType ) ;
+			break ;
+**/
+		default :
+			cerr << "Invalid message configuration file section" << std::endl;
+			success = false ;
+			break ;
+	}
+
+	return true ;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//#BEGIN
+//#METHOD bool SmsFilePaMgr::AnalyseMsgTypeLine( RWCString line )
+//#COMMENT Analyse line as message type description line. If it succeeds, send message type creation command to server
+// application. If it fails, try to analyse this same line as message variable description line.
+//#END
+//----------------------------------------------------------------------------------------------------------------------
+bool SmsFilePaMgr::AnalyseMsgTypeLine( string line )
+{
+	//cerr <<	"SmsFilePaMgr::AnalyseMsgTypeLine" << std::endl;
+
+	// Retrieve message type description line format
+
+	string format = "%2d%*1[	]%200c" ; //TODO SmsUpdatePa::Get()->getParameters()->getMsgTypeLineFormat() ;
+	int msgTypeId = 0 ;
+	char msgTypeName[256] ;
+	memset( msgTypeName, 0, sizeof( msgTypeName ) ) ;
+
+
+
+	// Analyse line as message type line
+	if ( sscanf( line.data(), format.data(), &msgTypeId, msgTypeName ) < 2 )
+	{
+		//cerr <<	"Cannot comprehend: " << line << std::endl;
+		return false;
+		//TODO : AND how to remove the recursive function return AnalyseMsgConfigLine( line, PA_SECTION_MSG_VAR ) ;
+	}
+
+	// Checks message type id validity
+	if ( ( msgTypeId < PA_MIN_MSG_TYPE_ID )||( msgTypeId > PA_MAX_MSG_TYPE_ID ) )	//PA_MAX_MSG_TYPE_ID: 10 exclude INSTANT Msg
+	{
+		cerr << "*** Invalid message type id %d ***" << msgTypeId  << std::endl;
+		return false ;
+	}
+
+	cerr << std::endl;
+	cerr << "<" << msgTypeId << ">"<< " MESSAGE TYPE LINE: " << msgTypeName << std::endl;//tmp by sam
+	/**	TODO output as RESTful API
+	RWCString type = msgTypeName ;
+	SmsFileAnalyser::ExtractSpaces( type ) ;
+
+	// Create and send command
+	SendCommand( PA_CMD_MSG_TYPE_CREATION, type.data(), msgTypeId ) ;
+
+	trace.printn( "Create message type %s : <id>%d", type.data(), msgTypeId ) ;
+
+	**/
+	return true ;
+}
+
+
 class UrlPaService: public Poco::OSP::Auth::PaService
 	/// A very simple implementation of AuthService.
 	///
@@ -130,10 +323,83 @@ public:
  *	@call: 	getFile				:	FTP the latest PA text file: instant.txt, database.txt
  *	@TODO :		-instant message: 	"props.set"
  *	@step:	decoding the file and get the message (1. instant.txt	2. database.txt [PA_SECTION_PREDEF_MSG])
+ *	@step:	decoding the file	:	SmsFilePaMgr::AnalyseMsgConfigLine
  * */
 	void reconnect(Poco::Util::TimerTask&)
 	{
 		cerr << "UrlPaService reconnect start" <<	endl;
+
+#define PA_ReadSchedule
+
+
+#ifdef PA_ReadSchedule
+			//TODO get the PA schedule file
+			//TODO decode the file				SmsFilePaMgr::DecodeMsgCode
+			//TODO map to the PA zone
+			//TODO update to the MMI (Start Time , message #10.01#, comment )	SmsPa::mhqueueLineCreation -> SmsPa::getMsg
+
+			/**	SW_Requirement_Specification
+			 * 	7	description of Scheduler file
+			 *				startTime	EndTime		repetitionField
+			 *													Message Code
+			 *													zoneAddress
+			 *														messageField
+			 *														        PIDSCode[16]	    Comment[35]                                          Comment[35]?                   RepID	SLineID
+			 *																																														Station Code
+			 */
+
+			string localfile = "/Users/sms/msgConfig.txt", remotefile = "PAServer/config/database.txt";
+			//tmp by sam without network getFile(localfile, remotefile, "anonymous", "sms", "128.13.22.246");
+			if(!SmsFilePaMgr::ReadMsgConfig(localfile.c_str())){
+				cerr <<  "*** Read message config error ***" << endl;
+			}
+/**
+ * 			TODO
+ * 			read the file						:	SmsFilePaMgr::ReadMsgConfig( const char *fileName )
+
+ * 			decode and store the file			:	SmsFilePaMgr::AnalyseMsgConfigLine
+ 			01	Select
+			02	Service
+			04	Emer
+			05	Last
+			06	Auto
+			07	Courtesy
+			09	Crowd
+			10	Instant
+			#01     01C	.
+			#01     01E	one
+
+ *			mapping of the above msgConfigLine	:	SmsPa::mhqueueLineCreation
+  			string mhQueue1		=	"14:56:31	4078	ID	22	160	#X0000000000000007#07.31#                                    	00NNNNNNNNNNNNNN	S000S                              	R99P300";
+ 			string mhQueue2		= 	"14:56:53	4080	ID	22	160	#X0000000000000007#07.23#                                    	00NNNNNNNNNNNNNN	S000S                              	R99P300";
+ 			string mhQueue3		=	"14:55:38	4084	ID	22	140	#X0000000000000007#10.87#                                    	00NNNNNNNNNNNNNN	S000S                              	R99P300";
+ 			string mhQueue4		=	"14:56:05	4088	ID	22	140	#X0000000000000007#10.97#                                    	00NNNNNNNNNNNNNN	S000S                              	R99P300";
+
+**/
+			string scheduleline = 	"12:34:56	  :  :  	once	   	#009#10.01#                                                  00NNNNNNNNNNNNNN	                                   	0000	4355	S000S                              ";
+			/**
+			 * TODO					"Time					Loop		Zone;Msg Name: (05) Last   (01) Last train at platform (01) +ONE is the last train for (07) Mei Foo, board immediately
+			string todoScheLine = 	"00:34:00	  :  :  	once	   	#099#05.01.01.07#                                            00NNNNNNNNNNNNNN	                                   	0000	9025	S000S                            ";
+			 */
+
+			string scheduleformat = "%8c%*1[	]%*8c%*1[	]%*4c%*1[	]%*3c%*1[	]%61c%*16c%*1[	]%35c%*1[	]%4d%*1[	]%4d%*1[	]%*35c";
+
+			char startTime[256] ;
+			char msgCode[256] ;
+			bool isPhysicalAddress ;
+			char commentStr[256] ;
+			int repetitionId ;
+			int lineId ;
+
+			//int returnValue = sscanf( line.data(), format.data(), msgType, &msgId, &msgPart, chineseOrEnglish, &varType, msgText );
+			int resSscanf = sscanf( scheduleline.data(), scheduleformat.data(), startTime, msgCode, commentStr, &repetitionId, &lineId ) ;
+
+			printf("<%s>, <%s>, <%s>, <%d>, <%d>: return %d\n", startTime, msgCode, commentStr, &repetitionId, &lineId, resSscanf);
+
+#endif
+
+
+
 
 		int err = 0;
 		int index = 0;
@@ -180,25 +446,32 @@ public:
 		*	TODO <7> how to display in the MMI	:	w.r.t. SmsPanelPa::editSelectorCB	//instant messages editor
 		*
 		***/
+
+//#define PA_SECTION_MSG_TYPE_CODE
+//#define PA_SECTION_MSG_VAR_CODE
+#define PA_SECTION_PREDEF_MSG_CODE	//tmp by sam 20180124
+
+
+#if defined(PA_SECTION_MSG_TYPE_CODE) || defined(PA_SECTION_MSG_VAR_CODE) || defined(PA_SECTION_PREDEF_MSG_CODE)
+
+#endif
+
 		if(true){ //TODO <5> in case of status receiveMsg[??] for updating configuration file
 			string localfilename = "/Users/sms/aDatabase.txt", remotefilename = "PAServer/config/database.txt";
 			getFile(localfilename, remotefilename, "anonymous", "sms", "128.13.22.246");
 
 //TODO by sam to generalize the following as functions
 
-//#define PA_SECTION_MSG_TYPE
-//#define PA_SECTION_MSG_VAR
-#define PA_SECTION_PREDEF_MSG
 
-#ifdef PA_SECTION_MSG_TYPE
+#ifdef PA_SECTION_MSG_TYPE_CODE
 			/**
 			string line	= "07	Courtesy";	**/							//PA_SECTION_MSG_TYPE	SmsPa::msgTypeCreation
 			string line = "09	Crowd";
 #endif
-#ifdef PA_SECTION_MSG_VAR
+#ifdef PA_SECTION_MSG_VAR_CODE
 			string line 	= "#03     01E	this";						//PA_SECTION_MSG_VAR
 #endif
-#ifdef PA_SECTION_PREDEF_MSG
+#ifdef PA_SECTION_PREDEF_MSG_CODE
 			/**
 			string line 	= "Last    01.2E #03	is the last train for";	//PA_SECTION_PREDEF_MSG	: 	SmsPa::predefMsgPartCreation
 			string line 	= "Service 05.1E #03	Reduced service due to incident at a station: Your attention please, Train service to";	**/
@@ -207,28 +480,28 @@ public:
 
 
 			//TODO	SmsUpdatePa::Get()->getParameters()->getMsgTypeLineFormat() ;
-#ifdef PA_SECTION_MSG_TYPE
+#ifdef PA_SECTION_MSG_TYPE_CODE
 			string format = "%2d%*1[	]%200c";				//PA_SECTION_MSG_TYPE
 #endif
-#ifdef PA_SECTION_MSG_VAR
+#ifdef PA_SECTION_MSG_VAR_CODE
 			/** by sam originally
 			string format	= "%8c%2d%[CE]%*1[	]%200c";		//PA_SECTION_MSG_VAR	: msgWithoutVarLineFormat **/
 			string format	= "#%2d%*1[     ]%2d%[CE]%*1[	]%200c";		//PA_SECTION_MSG_VAR	: msgWithoutVarLineFormat
 #endif
-#ifdef PA_SECTION_PREDEF_MSG
+#ifdef PA_SECTION_PREDEF_MSG_CODE
 			string format 	= "%8c%2d.%1d%[CE]%*1[ ]#%2d%*1[	]%200c";	//PA_SECTION_PREDEF_MSG	: msgWithVarLineFormat
 			/**
 			string format 	= "%8c%2d.%1d%[CE]%*1[	]%200c"; 				//						: msgWithVarEndLineFormat**/
 #endif
 
 
-#ifdef PA_SECTION_MSG_TYPE
+#ifdef PA_SECTION_MSG_TYPE_CODE
 			//PA_SECTION_MSG_TYPE
 			int msgTypeId = 0;
 			char msgTypeName[256] ;
 			memset( msgTypeName, 0, sizeof( msgTypeName ) ) ;
 #endif
-#ifdef PA_SECTION_MSG_VAR
+#ifdef PA_SECTION_MSG_VAR_CODE
 			//PA_SECTION_MSG_VAR
 			int varTypeId = 0 ;
 			int varId = 0 ;
@@ -237,7 +510,7 @@ public:
 			char varText[256] ;
 			memset( varText, 0, sizeof( varText ) ) ;
 #endif
-#ifdef PA_SECTION_PREDEF_MSG
+#ifdef PA_SECTION_PREDEF_MSG_CODE
 			//PA_SECTION_PREDEF_MSG	: msgWithVarEndLineFormat
 			char msgType[256] ;
 			memset( msgType, 0, sizeof( msgType ) ) ;
@@ -250,24 +523,24 @@ public:
 			memset( msgText, 0, sizeof( msgText ) ) ;
 #endif
 			// Analyse line as message type line	by int sscanf ( const char * s, const char * format, ...);
-#ifdef PA_SECTION_MSG_TYPE
+#ifdef PA_SECTION_MSG_TYPE_CODE
 			int returnValue = sscanf(line.c_str(), format.c_str(), &msgTypeId, msgTypeName);						//PA_SECTION_MSG_TYPE
 #endif
-#ifdef PA_SECTION_MSG_VAR
+#ifdef PA_SECTION_MSG_VAR_CODE
 			int returnValue = sscanf( line.data(), format.data(), &varTypeId, &varId, chineseOrEnglish, varText );	//PA_SECTION_MSG_VAR
 #endif
-#ifdef PA_SECTION_PREDEF_MSG
+#ifdef PA_SECTION_PREDEF_MSG_CODE
 			int returnValue = sscanf( line.data(), format.data(), msgType, &msgId, &msgPart, chineseOrEnglish, &varType, msgText );
 #endif
 
 			cerr << line << endl;
-#ifdef PA_SECTION_MSG_TYPE
+#ifdef PA_SECTION_MSG_TYPE_CODE
 			printf("%s -> %d return %d\n", msgTypeName, msgTypeId, returnValue);	//PA_SECTION_MSG_TYPE
 #endif
-#ifdef PA_SECTION_MSG_VAR
+#ifdef PA_SECTION_MSG_VAR_CODE
 			printf("%d, %d, %s, %s return %d\n", varTypeId, varId, chineseOrEnglish, varText, returnValue);	//PA_SECTION_MSG_VAR
 #endif
-#ifdef PA_SECTION_PREDEF_MSG
+#ifdef PA_SECTION_PREDEF_MSG_CODE
 			printf("%s, %d, %d, %s, %d: <%s> return %d\n", msgType, msgId, msgPart, chineseOrEnglish, varType, msgText, returnValue);	//PA_SECTION_MSG_VAR
 #endif
 //TODO by sam to generalize the following as functions
